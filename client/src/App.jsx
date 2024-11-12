cdd99b6d899b2291b42d2d311fbc988548d6c233
Fary~ <65035293+fary58@users.noreply.github.com>
2024-11-12 23:51:21 +0000

    navbar & sidebar added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Inbox from './components/Inbox'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-
-    </>
+    <div className='bg-[#F6F8FC] h-screen'>
+      <Navbar/>
+      <div className='flex'>
+      <Sidebar/>
+      <Inbox/>
+      </div>
+    </div>
   )
 }
 
