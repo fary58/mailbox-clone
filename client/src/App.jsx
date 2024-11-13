2c32a1d9b94cc6261fbb480a543b6de871bf941a
Fary~ <65035293+fary58@users.noreply.github.com>
2024-11-13 06:05:06 +0000

    mail body added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Inbox from './components/Inbox'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
-
+import Body from './components/Body'
 
 function App() {
   return (
