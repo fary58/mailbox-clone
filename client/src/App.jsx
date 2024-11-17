a9748dfc03e56e506e74033a03053c418dd47552
Fary~ <65035293+fary58@users.noreply.github.com>
2024-11-17 05:24:45 +0000

    routes added & ui fixed for app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,38 @@
 
 import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Inbox from './components/Inbox'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import Body from './components/Body'
+import Mail from './components/Mail'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <Inbox />
+      },
+      {
+        path: "/mail/:id",
+        element: <Mail />
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <div className='bg-[#F6F8FC] h-screen'>
-
-    </div>
+      <RouterProvider router={router} />
+      </div>
   )
 }
 
--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import Sidebar from './Sidebar'
-import { useSelector } from 'react-redux';
 import Navbar from './Navbar';
 
 const Body = () => {
