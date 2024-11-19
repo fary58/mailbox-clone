5d168e3d6933a99f9a3ddda98db3f7ca1c1f2dd0
Fary~ <65035293+fary58@users.noreply.github.com>
2024-11-19 06:36:15 +0000

    database connection added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
-
+const cors = require("cors");
+var bodyParser = require("body-parser");
 const app = express();
+require("dotenv").config();
+app.use(cors());
+
+
+
+const connectDB = require("./db/connectionDB");
+connectDB();
+
+
 const PORT = 8080;
 app.listen(PORT, ()=>{
     console.log(`Server running at port ${PORT}`);
