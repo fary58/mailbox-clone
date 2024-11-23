9b2e443ea64ef032a37fe60b2608ddc76b7c994c
Fary~ <65035293+fary58@users.noreply.github.com>
2024-11-23 23:45:49 +0000

    userController created with register function

---
+++ server/controllers/userController.js
@@ -0,0 +1,31 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+    try {
+        const {fullname, email, password} = req.body;
+        if(!fullname || !email || !password) return res.status(400).json({message:"All fields are required", success:false});
+
+        const user = await User.findOne({email});
+
+        if(user) return res.status(400).json({message:"User already exist with this email", success:false});
+        
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const profilePhoto = `https://avatar.iran.liara.run/public/boy`;
+        await User.create({
+            fullname, 
+            email,
+            password:hashedPassword,
+            profilePhoto
+        });
+
+        return res.status(201).json({
+            message:"Account created successfully.",
+            success:true
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
