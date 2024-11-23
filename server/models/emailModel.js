995f58a47c1d08dce4b1d886fb9016ed68ab8434
Fary~ <65035293+fary58@users.noreply.github.com>
2024-11-23 23:40:39 +0000

    models created

---
+++ server/models/emailModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+
+const emailSchema = new mongoose.Schema({
+    to:{
+        type:String,
+        required:true
+    },
+    subject:{
+        type:String,
+        required:true
+    },
+    message:{
+        type:String,
+        required:true
+    },
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    }
+},{timestamps:true});
+export const Email = mongoose.model("Email", emailSchema);
