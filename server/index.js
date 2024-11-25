bdafe3f37cdbea2f1bef33280b154e3caffdcf4e
Fary~ <65035293+fary58@users.noreply.github.com>
2024-11-25 06:29:25 +0000

    server side issues fixed

--- server/index.js
+++ server/index.js
@@ -1,17 +1,25 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 var bodyParser = require("body-parser");
 const app = express();
 require("dotenv").config();
 app.use(cors());
 
-
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
 
 const connectDB = require("./db/connectionDB");
 connectDB();
-
-
 const PORT = 8080;
-app.listen(PORT, ()=>{
-    console.log(`Server running at port ${PORT}`);
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
 });
