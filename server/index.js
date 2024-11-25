d36eafffbd0f50df37e8db5d830d525daa31e9bb
Fary~ <65035293+fary58@users.noreply.github.com>
2024-11-25 06:38:55 +0000

    jwt authentication added & userRoutes finished

--- server/index.js
+++ server/index.js
@@ -5,20 +5,26 @@ const app = express();
 require("dotenv").config();
 app.use(cors());
 
+const connectDB = require("./db/connectionDB");
+connectDB();
+const PORT = 8080;
+
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-const connectDB = require("./db/connectionDB");
-connectDB();
-const PORT = 8080;
+
 
 const corsOptions = {
   origin: "http://localhost:5173",
   credentials: true,
 };
 app.use(cors(corsOptions));
+
+
+// routes
+app.use("/api/v1/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
